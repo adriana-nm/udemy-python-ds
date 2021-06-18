2404e8e5575f52871aab7c858a317f8e5de432f6
adrinmdq <74638802+adrinmdq@users.noreply.github.com>
2021-06-17 23:51:33 -0300

    Excercise_Salaries Update

--- Pandas/Exercises_Salaries.py
+++ Pandas/Exercises_Salaries.py
@@ -32,16 +32,23 @@ sal[sal['EmployeeName']=='JOSEPH DRISCOLL']['TotalPayBenefits']
 sal.iloc[sal['TotalPayBenefits'].idxmax()]['EmployeeName']
 
 #Name of lowest paid person (including benefits)
-
+sal.iloc[sal['TotalPayBenefits'].idxmin()]['EmployeeName']
 
 #Average(mean) BasePay of all employees per year? (2011-2014)?
+sal.groupby('Year').mean('BasePay')['BasePay']
 
 #How many unique job titles are there?
+sal['JobTitle'].nunique()
 
 #Top 5 most common jobs?
+sal.groupby('JobTitle').count().sort_values(by='Id',axis=0,ascending=False,).iloc[0:5].filter(['JobTitle','Id'])
 
 #How many Job Titles were represented by only one person in 2013? (e.g. Job Titles with only one occurence in 2013?)
+sal.query('Year == 2013').groupby('JobTitle').count().query('Id == 1').count
+
 
 #How many people have the word Chief in their job title?
 
+
 #Is there a correlation between length of the Job Title string and Salary?
+
